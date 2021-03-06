7d451440b8a5ed7e9b8a1f038c3660ee7ad73a70
Sathyapriyan Chinnathangam Nadar <nsathya1990@gmail.com>
2021-03-06 23:43:21 +0530

    redux and ui state

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -8,24 +8,30 @@ const controls = [
     { label: 'Salad', type: 'salad' },
     { label: 'Bacon', type: 'bacon' },
     { label: 'Cheese', type: 'cheese' },
-    { label: 'Meat', type: 'meat' }
+    { label: 'Meat', type: 'meat' },
 ];
 
-const buildControls = props => (
+const buildControls = (props) => (
     <div className={classes.BuildControls}>
-        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
-        {controls.map(ctrl => (
+        <p>
+            Current Price: <strong>{props.price.toFixed(2)}</strong>
+        </p>
+        {controls.map((ctrl) => (
             <BuildControl
                 key={ctrl.label}
                 label={ctrl.label}
                 added={() => props.ingredientAdded(ctrl.type)}
                 removed={() => props.ingredientRemoved(ctrl.type)}
-                disabled={props.disabled[ctrl.type]} />
+                disabled={props.disabled[ctrl.type]}
+            />
         ))}
         <button
             className={classes.OrderButton}
-            // disabled={!props.purchaseable}
-            onClick={props.ordered}>ORDER NOW</button>
+            disabled={!props.purchasable}
+            onClick={props.ordered}
+        >
+            ORDER NOW
+        </button>
     </div>
 );
 
