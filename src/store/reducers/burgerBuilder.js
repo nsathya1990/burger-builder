fe3854911208133cc6853d124d33951664855af1
Sathyapriyan Chinnathangam Nadar <nsathya1990@gmail.com>
2021-03-11 02:18:57 +0530

    changing the order of our ingredients manually

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -3,7 +3,7 @@ import * as actionTypes from '../actions/actionTypes';
 const initialState = {
     ingredients: null,
     totalPrice: 4,
-    error: false
+    error: false,
 };
 
 const INGREDIENT_PRICES = {
@@ -40,13 +40,18 @@ const reducer = (state = initialState, action) => {
         case actionTypes.SET_INGREDIENTS:
             return {
                 ...state,
-                ingredients: action.ingredients,
-                error: false
+                ingredients: {
+                    salad: action.ingredients.salad,
+                    bacon: action.ingredients.bacon,
+                    cheese: action.ingredients.cheese,
+                    meat: action.ingredients.meat,
+                },
+                error: false,
             };
         case actionTypes.FETCH_INGREDIENTS_FAILED:
             return {
                 ...state,
-                error: true
+                error: true,
             };
         default:
             return state;
