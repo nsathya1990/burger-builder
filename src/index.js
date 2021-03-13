3cbe67191a38a5c1ae6eebd71d1b4d5ef84d3440
Sathyapriyan Chinnathangam Nadar <nsathya1990@gmail.com>
2021-03-13 23:43:12 +0530

    adding lazy loading

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 
 import './index.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
