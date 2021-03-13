3dce1d38faf4ea67a6d3fd48fdcaa0764dc5ec8c
Sathyapriyan Chinnathangam Nadar <nsathya1990@gmail.com>
2021-03-13 20:14:23 +0530

    displaying user specific orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -11,18 +11,14 @@ import * as actions from '../../store/actions';
 
 class Orders extends Component {
     componentDidMount() {
-        this.props.onFetchOrders(this.props.token);
+        this.props.onFetchOrders(this.props.token, this.props.userId);
     }
 
     render() {
         let orders = <Spinner />;
         if (!this.props.loading) {
             orders = this.props.orders.map((order) => (
-                <Order
-                    key={order.id}
-                    ingredients={order.ingredients}
-                    price={order.price}
-                />
+                <Order key={order.id} ingredients={order.ingredients} price={order.price} />
             ));
         }
         return <div>{orders}</div>;
@@ -33,17 +29,15 @@ const mapStateToProps = (state) => {
     return {
         orders: state.order.orders,
         loading: state.order.loading,
-        token: state.auth.token
+        token: state.auth.token,
+        userId: state.auth.userId,
     };
 };
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onFetchOrders: (token) => dispatch(actions.fetchOrders(token)),
+        onFetchOrders: (token, userId) => dispatch(actions.fetchOrders(token, userId)),
     };
 };
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(withErrorHandler(Orders, axios));
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(Orders, axios));
