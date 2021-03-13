dfde934c8b50f110e72740c5139bc90e6be5f8e2
Sathyapriyan Chinnathangam Nadar <nsathya1990@gmail.com>
2021-03-13 21:28:16 +0530

    using updateObject in the entire app

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -8,6 +8,8 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 
 import * as actions from '../../store/actions/index';
 
+import { updateObject } from '../../shared/utility';
+
 import classes from './Auth.css';
 
 class Auth extends Component {
@@ -66,18 +68,16 @@ class Auth extends Component {
     }
 
     inputChangedHandler = (event, controlName) => {
-        const updatedControls = {
-            ...this.state.controls,
-            [controlName]: {
-                ...this.state.controls[controlName],
+        const updatedControls = updateObject(this.state.controls, {
+            [controlName]: updateObject(this.state.controls[controlName], {
                 value: event.target.value,
                 valid: this.checkValidity(
                     event.target.value,
                     this.state.controls[controlName].validation
                 ),
                 touched: true,
-            },
-        };
+            }),
+        });
         this.setState({ controls: updatedControls });
     };
 
@@ -164,8 +164,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup)),
-        onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/'))
-
+        onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/')),
     };
 };
 
