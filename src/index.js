ee36eded2632277b1c02e4db38ede8b118af786f
Sathyapriyan Chinnathangam Nadar <nsathya1990@gmail.com>
2021-03-13 10:47:31 +0530

    storing the token

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import authReducer from './store/reducers/auth';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
 
@@ -17,12 +18,10 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
     order: orderReducer,
+    auth: authReducer,
 });
 
-const store = createStore(
-    rootReducer,
-    composeEnhancers(applyMiddleware(thunk))
-);
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
     <Provider store={store}>
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -10,10 +10,11 @@ export const authStart = () => {
     };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
     return {
         type: actionTypes.AUTH_SUCCESS,
-        authData,
+        idToken: token,
+        userId,
     };
 };
 
@@ -41,7 +42,7 @@ export const auth = (email, password, isSignup) => {
             .post(url, authData)
             .then((response) => {
                 console.log(response);
-                dispatch(authSuccess(response.data));
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
             })
             .catch((err) => {
                 console.log(err);
